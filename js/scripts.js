b848f3e2338d9d513da39f83b649c6445aebe0ef
kendra ash & alisa colon <student@epicodus.com>
2015-09-08 16:01:04 -0700

    add jQuery for the browser to display and allow user input

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,17 @@ var factorial = function(number) {
     return total;
   }
 };
+
+
+$(document).ready(function() {
+  $("form#factorial").submit(function(event) {
+    var number = parseInt($("input#number").val());
+    var result = factorial(number);
+
+    $(".number").text(number);
+    $(".result").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
